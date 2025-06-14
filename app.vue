4b745b7603bfab5cb41947ef380426a20db788a1
Arsam Sarabi <asarabi@oni.bio>
2025-06-14 14:42:25 +0100

    Add landing page

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl">
-      Hello Nuxt
-    </h1>
-    <button class="btn btn-soft btn-secondary">
-      <Icon name="tabler:map" /> View Map
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ components/app/nav-bar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink class="btn btn-ghost text-xl">
+        TL
+      </NuxtLink>
+    </div>
+
+    <div class="navbar-end">
+      <NuxtLink class="btn btn-accent" to="/sign-in">
+        <Icon name="tabler:brand-github" />
+        Sign In with Github
+      </NuxtLink>
+    </div>
+  </div>
+</template>
