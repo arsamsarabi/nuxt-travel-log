08c11d1c4521176ab3dbe70eafe2d61187e58598
Arsam Sarabi <arsamsarabi@me.com>
2025-06-14 15:03:49 +0100

    Add theme toggle component (#51)

---
+++ components/app/theme-toggle.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value: boolean) {
+    colorMode.preference = value ? "dark" : "light";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate">
+    <!-- this hidden checkbox controls the state -->
+    <input v-model="isDark" type="checkbox">
+
+    <Icon class="swap-on" name="tabler:sun-high" size="24" />
+    <Icon class="swap-off" name="tabler:moon-stars" size="24" />
+  </label>
+</template>
